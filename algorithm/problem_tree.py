c69f45d3dbbb0ebe9a0775203cd86dc18d1a41e4
Isaac Rudnick <48895941+IsaacFigNewton@users.noreply.github.com>
2024-07-06 18:22:42 -0700

    Refactored code, added time-saving functions
    Refactored token gathering and optimal child search code
    Added optional branch pruning (for memory saving)
    Added token frequency table caching (dynamic programming)

--- algorithm/problem_tree.py
+++ algorithm/problem_tree.py
@@ -1,8 +1,14 @@
-# the root node is the frequency table of all 1-grams in the text
-# the children of the root node are frequency tables of all possible 2-grams
-
 from itertools import chain, combinations
 from collections import Counter
+import json
+
+# whether or not to prune dead branches of the tree/save them for later printing/debugging
+prune_tree = 1
+
+# a dictionary of previously explored frequency tables at each depth
+# Key:      problem tree depth
+# Value:    set of frequency table dictionaries at that depth
+token_freq_tables = dict()
 
 class TokenSetNode:
     # keep track of the deepest child and the height of the tree to determine the best token set
@@ -22,9 +28,12 @@ class TokenSetNode:
         # keep track of the current node's depth in the problem tree
         self.depth = depth
 
+    # T(n) = TokenSetNode.deepest_child
     def __eq__(self, other):
         if isinstance(other, self.__class__):
-            return self.token_freqs == other.token_freqs and self.depth == other.depth
+            return self.token_freqs == other.token_freqs\
+                and self.depth == other.depth\
+                and self.parent == other.parent
         return False
 
     def __lt__(self, other):
@@ -32,61 +41,31 @@ class TokenSetNode:
             return self.depth < other.depth
         return False
 
+def check_token_freq_tables(freq_table, depth):
+    # convert the frequency table to a hashable type by treating it as json to be flattened
+    json_str = json.dumps(freq_table, sort_keys=True)
 
-def filter_n1_tokens(n1_tokens):
-    # print("n + 1 gram tokens before filtering: ", n1_tokens)
+    # if the set of tables at this depth has been defined
+    if depth in token_freq_tables.keys():
+        # if this branch has been previously explored
+        if json_str in token_freq_tables[depth]:
+            return True
+        # otherwise, mark this branch as explored, but continue deeper
+        else:
+            token_freq_tables[depth].add(json_str)
+            return False
+    # otherwise, if nothing has been explored at this depth before
+    else:
+        # create a new entry for the branch at this depth
+        token_freq_tables[depth] = set()
+        # mark it as explored
+        token_freq_tables[depth].add(json_str)
+        # continue
+        return False
 
-    # TODO: implement this for n-grams larger than 1 in a time, space efficient manner
-    n1_gram_tokens = {token for token in n1_tokens
-                      if all(one_gram not in token for one_gram in TokenSetNode.one_grams_to_exclude)}
-
-    # print("n + 1 gram tokens after filtering: ", n1_gram_tokens)
-    # print()
-
-    return n1_gram_tokens
 
 
-# generate the powerset of the n+1-gram tokens (excluding the empty set)
-def powerset(iterable):
-    s = list(iterable)
-    return chain.from_iterable(combinations(s, r) for r in range(1, len(s) + 1))
-
-
-# recursively build the problem tree
-def create_prob_tree(token_freqs, text="", parent=None, depth=0):
-    """
-    Creates a problem tree from token frequencies.
-
-    Args:
-        token_freqs (dict): A dictionary of token frequencies.
-        parent (TokenSetNode, optional): The parent node. Defaults to None.
-        depth (int, optional): The current depth of the tree. Defaults to 0.
-        text (str, optional): The original text. Defaults to None.
-
-    Returns:
-        TokenSetNode: The root of the problem tree.
-    """
-    # initialize the root node with the current token frequencies, parent, and depth
-    root = TokenSetNode(token_freqs, parent, depth)
-
-    # if no text was provided
-    if len(text) <= 1:
-        print("no text provided")
-        return root
-    # if no token frequencies were provided
-    elif (not token_freqs):
-        print("no token frequencies provided")
-        return root
-
-    # get the length of the longest token
-    max_token_length = max(len(token) for token in token_freqs)
-    # filter out tokens with the maximum length and frequency > 1
-    base_set = {token for token in token_freqs if len(token) == max_token_length and token_freqs[token] > 1}
-
-    # if the frequencies of the largest tokens are all 1
-    if len(base_set) == 0:
-        return root
-
+def collect_n1_grams(base_set, text):
     # create a set to hold the n+1-gram tokens
     n1_gram_tokens = set()
 
@@ -115,65 +94,170 @@ def create_prob_tree(token_freqs, text="", parent=None, depth=0):
             # move to the next appearance
             start += len(token)
 
-    # filter out tokens that intersect with a smaller token that has frequency = 1
-    n1_gram_tokens = filter_n1_tokens(n1_gram_tokens)
+    return n1_gram_tokens
+
+
+def count_substrings(string, substrings):
+    # Initialize a Counter object
+    counter = Counter()
+
+    # Iterate through each substring
+    for substring in substrings:
+        # Count the occurrences of the substring in the main string
+        count = string.count(substring)
+        # Update the Counter object with the count
+        counter[substring] = count
+
+    return counter
+
+
+def filter_n1_tokens(n1_tokens, text):
+    # print("n + 1 gram tokens before filtering: ", n1_tokens)
+
+    # TODO: implement this for n-grams larger than 1 in a time, space efficient manner
+    n1_gram_tokens = {token for token in n1_tokens
+                      if all(one_gram not in token for one_gram in TokenSetNode.one_grams_to_exclude)}
+
+    n1_freqs = count_substrings(text, n1_gram_tokens)
+    n1_gram_tokens = {token for token in n1_freqs if n1_freqs[token] > 1}
+
+    # print("n + 1 gram tokens after filtering: ", n1_gram_tokens)
+    # print()
+
+    return n1_gram_tokens
+
+
+# generate the powerset of the n+1-gram tokens (excluding the empty set)
+def powerset(iterable):
+    s = list(iterable)
+    return chain.from_iterable(combinations(s, r) for r in range(1, len(s) + 1))
+
+
+def prune_dead_branch(node):
+    if prune_tree:
+        if node is None:
+            return
+
+        # If the node is a leaf (has no children) and is not the current deepest child
+        if len(node.children) == 0 and node != TokenSetNode.deepest_child:
+            if node.parent:
+                # Remove this node from its parent's children
+                if node in node.parent.children:
+                    node.parent.children.remove(node)
+                # Recursively check the parent
+                prune_dead_branch(node.parent)
+
+    pass
+
+
+def update_deepest_child(root, depth):
+    new_child = TokenSetNode({}, root, depth + 1)
+
+    # Prune the old deepest child if it exists and is different from the new one
+    if TokenSetNode.deepest_child and TokenSetNode.deepest_child != new_child:
+        old_deepest = TokenSetNode.deepest_child
+        TokenSetNode.deepest_child = new_child
+        prune_dead_branch(old_deepest)
+
+    TokenSetNode.height = depth + 1
+    TokenSetNode.deepest_child = new_child
+    root.children.append(new_child)
+
+
+# recursively build the problem tree
+def create_prob_tree(token_freqs, text="", parent=None, depth=0):
+    """
+    Creates a problem tree from token frequencies.
+
+    Args:
+        token_freqs (dict): A dictionary of token frequencies.
+        parent (TokenSetNode, optional): The parent node. Defaults to None.
+        depth (int, optional): The current depth of the tree. Defaults to 0.
+        text (str, optional): The original text. Defaults to None.
+
+    Returns:
+        TokenSetNode: The root of the problem tree.
+    """
+
+    # initialize the root node with the current token frequencies, parent, and depth
+    root = TokenSetNode(token_freqs, parent, depth)
+
+    # check if this branch has been previously explored
+    previously_explored = check_token_freq_tables(token_freqs, depth)
+    # if it has, then stop here
+    if previously_explored:
+        return root
+    # if no text was provided
+    if len(text) <= 1:
+        print("no text provided")
+        return root
+    # if no token frequencies were provided
+    elif not token_freqs:
+        print("no token frequencies provided")
+        return root
+
+    # get the length of the longest token
+    max_token_length = max(len(token) for token in token_freqs)
+    # filter out tokens with the maximum length and frequency > 1
+    base_set = {token for token in token_freqs if len(token) == max_token_length and token_freqs[token] > 1}
+
+    # if the frequencies of the largest tokens are all 1
+    if len(base_set) == 0:
+        return root
+
+    # collect a set of n + 1 grams from the text
+    n1_gram_tokens = collect_n1_grams(base_set, text)
+
+    # filter tokens that intersect with a smaller token that has frequency = 1 or that only occur once
+    n1_gram_tokens = filter_n1_tokens(n1_gram_tokens, text)
     token_powerset = {i for i in powerset(n1_gram_tokens)}
     # print("Power set length: ", len(token_powerset))
     # print("Power set contents: ", token_powerset)
 
     # if there were no valid n+1-gram tokens
     if len(token_powerset) == 0:
-        child = TokenSetNode({}, root, depth + 1)
 
+        update_deepest_child(root, depth)
         # print("Child depth: ", depth + 1)
         # print("Child token freqs: ", child.token_freqs)
-        TokenSetNode.height = depth + 1
-        TokenSetNode.deepest_child = child
-        root.children.append(child)
 
         return root
 
-    for subset in token_powerset:  # .union(base_set)):
-        # count frequencies of n-gram and n+1-gram tokens in the subset
+    for subset in token_powerset:
         child_token_freqs = {token: text.count(token) for token in subset}
 
-        # if the maximum frequency of an n+1-gram is 1 or the
+        # if the most frequent token occurs only once, this is a leaf
         if max(child_token_freqs.values()) <= 1:
-            # make a leaf node
-            child = TokenSetNode(child_token_freqs, root, depth + 1)
-
-            # check if this is the deepest leaf
+            # if a new deepest child is found
             if depth + 1 > TokenSetNode.height:
-                # print("Child depth: ", depth + 1)
-                # print("Child token freqs: ", child.token_freqs)
-                TokenSetNode.height = depth + 1
-                TokenSetNode.deepest_child = child
-
-            root.children.append(child)
+                update_deepest_child(root, depth)
 
         else:
-            # recursively build the tree for deeper levels
             child = create_prob_tree(child_token_freqs, text, root, depth + 1)
-            root.children.append(child)
+            if child is not None:
+                root.children.append(child)
 
     return root
 
 
 def accumulate_token_set(node):
-    # get the current node's token set
-    token_set = dict(node.token_freqs)
+    if isinstance(node, TokenSetNode):
+        # get the current node's token set
+        token_set = dict(node.token_freqs)
 
-    # if this node has a parent, add on its token set and recurse
-    if node.parent != None:
-        token_set.update(accumulate_token_set(node.parent))
+        # if this node has a parent, add on its token set and recurse
+        if node.parent != None:
+            token_set.update(accumulate_token_set(node.parent))
+            return token_set
+
+        # return the combined token set
         return token_set
 
-    # return the combined token set
-    return token_set
+    print("Token accumulation failed")
+    return None
 
 
 def print_tree(node, indent=0):
-
     if node.depth == TokenSetNode.height:
         print('\t' * indent, "Depth ", node.depth, ", Token Frequencies: ",node.token_freqs, "\t<---- Deepest child")
     else:
@@ -195,10 +279,10 @@ def get_best_token_set(text):
     problem_tree = create_prob_tree(one_gram_freqs, text)
 
     print()
-    print_tree(problem_tree)
+    # print_tree(problem_tree)
     print()
 
-    best_token_set = accumulate_token_set(TokenSetNode.deepest_child)
+    best_token_set = accumulate_token_set(TokenSetNode.deepest_child.parent)
 
     print("Optimal token set: ", best_token_set)
     print("\n")
@@ -208,7 +292,7 @@ def get_best_token_set(text):
 
 
 if __name__ == "__main__":
-    token_set = get_best_token_set("aaaa")
+    token_set = get_best_token_set("mary had a little lamb, little lamb")
 
 
 
--- algorithm/tests.py
+++ algorithm/tests.py
@@ -6,12 +6,31 @@ class TestProbTree(unittest.TestCase):
     def test1(self):
         print("Testing 'aaaa'...")
         self.assertEqual(get_best_token_set("aaaa"),
-                    {'aaa': 1, 'aa': 2, 'a': 4})
+                    {'aa': 2, 'a': 4}
+        )
 
     def test2(self):
         print("Testing 'abcdabc'...")
         self.assertEqual(get_best_token_set("abcdabc"),
-                    {'abc': 2, 'ab': 2, 'a': 2, 'b': 2, 'c': 2, 'd': 1})
+                    {'abc': 2, 'ab': 2, 'a': 2, 'b': 2, 'c': 2, 'd': 1}
+        )
+
+    def test3(self):
+        print("Testing 'she sells seashells by the sea shore'...")
+        self.assertEqual(get_best_token_set("she sells seashells by the sea shore"),
+                    {'she': 2, 'he': 3, 'ea': 2, 'ls': 2, 'sh': 3, 's': 8, 'h': 4, 'e': 7, ' ': 6, 'l': 4, 'a': 2,
+                     'b': 1, 'y': 1, 't': 1, 'o': 1, 'r': 1}
+        )
+
+    def test4(self):
+        print("Testing 'mary had a little lamb, little lamb'...")
+        self.assertEqual(get_best_token_set("mary had a little lamb, little lamb, little lamb"),
+                    {' little lamb': 2, ' little lam': 2, ' little la': 2, ' little l': 2, ' little ': 2, 'little ': 2,
+                     'little': 2, 'ittle ': 2, ' littl': 2, 'ttle ': 2, 'littl': 2, ' lamb': 2, 'tle ': 2, ' lit': 2,
+                     'lamb': 2, 'litt': 2, 'e l': 2, 'amb': 2, 'lit': 2, 'itt': 2, 'le ': 2, 'li': 2, 'it': 2, 'tl': 2,
+                     'am': 2, 'e ': 2, 'mb': 2, 'm': 3, 'a': 5, 'r': 1, 'y': 1, ' ': 6, 'h': 1, 'd': 1, 'l': 6, 'i': 2,
+                     't': 4, 'e': 2, 'b': 2, ',': 1}
+        )
 
 if __name__ == "__main__":
     unittest.main()
